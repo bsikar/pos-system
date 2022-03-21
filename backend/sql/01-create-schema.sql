96c0b9f219edb898282238f6d1eee8c0c7612b46
Brighton Sikarskie <bsikar@tuta.io>
2022-03-21 15:01:11 -0500

    made sql uppercase

--- backend/sql/01-create-schema.sql
+++ backend/sql/01-create-schema.sql
@@ -1,15 +1,15 @@
 -- create entry
 CREATE TABLE IF NOT EXISTS purchase (
-    id bigserial NOT NULL, -- id of entry
-    ctime timestamp NOT NULL, -- creation time
-    items json NOT NULL, -- items in entry
-    total bigint NOT NULL -- amount of items in entry in cents
+    id BIGSERIAL NOT NULL, -- id of entry
+    ctime TIMESTAMP NOT NULL, -- creation time
+    items JSON NOT NULL, -- items in entry
+    total BIGINT NOT NULL -- amount of items in entry in cents
 );
 -- ALTER SEQUENCE purchase_id_seq RESTART WITH 1000; -- start id at 1000 so our test data wont overlap
 
 -- create entry
 CREATE TABLE IF NOT EXISTS item (
-    "name" text NOT NULL, -- name of item
-    price bigint NOT NULL, -- price of item in cents
-    tax real NOT NULL -- tax
+    "name" TEXT NOT NULL, -- name of item
+    price BIGINT NOT NULL, -- price of item in cents
+    tax REAL NOT NULL -- tax
 );
